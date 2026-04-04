c9720e2a5e7638c49dc56476e65838033f88988b
TapasPulis <rooneysamragya10@gmail.com>
2026-04-04 18:29:16 +0200

    Added a validation middleware

---
+++ src/middleware/validate.middleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { z } from "zod";
+
+export const validate =
+  (schema: z.ZodTypeAny) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    void res;
+    try {
+      // Throws error if invalid
+      await schema.parseAsync({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+      next(); // Valid! Continue.
+    } catch (error) {
+      next(error);
+    }
+  };
