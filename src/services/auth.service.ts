61e3812c0c7e8dcdc145b2148a7ea59c687f3983
TapasPulis <rooneysamragya10@gmail.com>
2026-04-06 19:10:52 +0200

    Added an auth service. This together with auth controller handles registering and logging a user

---
+++ src/services/auth.service.ts
@@ -0,0 +1,71 @@
+import type { RegisterUserTypeZ, LoginUserTypeZ } from "../models/auth.model";
+import bcrypt from "bcrypt";
+import jwt, { SignOptions } from "jsonwebtoken";
+import { prisma } from "../config/db";
+import { AppError } from "../utils/app.error";
+
+export const registerUserService = async (data: RegisterUserTypeZ) => {
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
+
+  if (existingUser) {
+    throw new AppError("User with this email already exists", 409);
+  }
+
+  const hashedPassword = await bcrypt.hash(data.password, 12);
+
+  return prisma.user.create({
+    data: {
+      email: data.email,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      password: hashedPassword,
+    },
+  });
+};
+
+export const loginUserService = async (data: LoginUserTypeZ) => {
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
+
+  if (!existingUser) {
+    throw new AppError("Invalid email or password", 401);
+  }
+
+  const isPasswordValid = await bcrypt.compare(
+    data.password,
+    existingUser.password,
+  );
+
+  if (!isPasswordValid) {
+    throw new AppError("Invalid email or password", 401);
+  }
+
+  const jwtSecret = process.env.JWT_SECRET;
+
+  if (!jwtSecret) {
+    throw new AppError("JWT_SECRET not set", 500);
+  }
+  const expiresIn = (process.env.JWT_EXPIRES_IN ??
+    "1d") as SignOptions["expiresIn"];
+
+  const token = jwt.sign(
+    {
+      id: existingUser.id,
+      email: existingUser.email,
+      role: existingUser.role,
+    },
+    jwtSecret,
+    {
+      expiresIn,
+    },
+  );
+
+  return { ...existingUser, token };
+};
