e53da33e6d0e96230ac672b6001208e1a640d94e
TapasPulis <rooneysamragya10@gmail.com>
2026-04-27 13:33:11 +0200

    Added validation error messages

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,10 +3,10 @@ import { z } from "zod";
 export const updateUserValidation = z.object({
   body: z
     .object({
-      email: z.string(),
-      password: z.string(),
-      firstName: z.string(),
-      lastName: z.string(),
+      email: z.string("Please enter a valid email").email(),
+      password: z.string("Please enter a password").min(6),
+      firstName: z.string("Please enter your first name").min(1),
+      lastName: z.string("Please enter your last name").min(1),
     })
     .partial()
     .strict(), // strict() will make sure that no extra fields are allowed in the body
