11228c89ae674f96cb6ad23e732ff1585740d9b7
TapasPulis <rooneysamragya10@gmail.com>
2026-05-28 13:56:57 +0200

    Corrected the cors url

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ export const createApp = () => {
 
   app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: "http://localhost:5173",
       methods: "GET, POST, PATCH, DELETE",
     }),
   );
